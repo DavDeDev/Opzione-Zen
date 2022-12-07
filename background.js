2e89000923701660e59c8443c2d72abff8f2736f
David02P <pietrocoladavid@gmail.com>
2022-12-07 18:30:46 -0500

    fix: improved readability and reusability

--- background.js
+++ background.js
@@ -3,62 +3,45 @@ chrome.runtime.onInstalled.addListener(() => {
     text: "OFF",
   });
 });
-const extensions = "https://developer.chrome.com/docs/extensions";
-const webstore = "https://developer.chrome.com/docs/webstore";
-const mdn = "https://developer.mozilla.org/en-US/docs/Web";
-const w3school ="https://www.w3schools.com/"
-
-
+const sites = {
+  google: "https://developer.chrome.com/docs/",
+  mdn: "https://developer.mozilla.org/en-US/docs/Web",
+  w3: "https://www.w3schools.com/",
+};
 chrome.action.onClicked.addListener(async (tab) => {
-  if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)  ) {
-    // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
-    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-    // Next state will always be the opposite
-    const nextState = prevState === "ON" ? "OFF" : "ON";
-
-    // Set the action badge to the next state
-    await chrome.action.setBadgeText({
-      tabId: tab.id,
-      text: nextState,
-    });
-    if (nextState === "ON") {
-      // Insert the CSS file when the user turns the extension on
-      await chrome.scripting.insertCSS({
-        files: ["focus-mode.css"],
-        target: { tabId: tab.id },
-      });
-    } else if (nextState === "OFF") {
-      // Remove the CSS file when the user turns the extension off
-      await chrome.scripting.removeCSS({
-        files: ["focus-mode.css"],
-        target: { tabId: tab.id },
-      });
+  for (const site in sites) {
+    if (tab.url.startsWith(sites[site])) {
+      var domain = site;
+      break;
     }
+    
   }
-  if (tab.url.startsWith(mdn)){
-    // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
-    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-    // Next state will always be the opposite
-    const nextState = prevState === "ON" ? "OFF" : "ON";
+  if (domain === undefined) {
+    return;
+  }
 
-    // Set the action badge to the next state
-    await chrome.action.setBadgeText({
-      tabId: tab.id,
-      text: nextState,
+  // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
+  const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+  // Next state will always be the opposite
+  const nextState = prevState === "ON" ? "OFF" : "ON";
+
+  // Set the action badge to the next state
+  await chrome.action.setBadgeText({
+    tabId: tab.id,
+    text: nextState,
+  });
+  if (nextState === "ON") {
+    // Insert the CSS file when the user turns the extension on
+    await chrome.scripting.insertCSS({
+      files: [`focus-mode-${domain}.css`],
+      target: { tabId: tab.id },
     });
-    if (nextState === "ON") {
-      // Insert the CSS file when the user turns the extension on
-      await chrome.scripting.insertCSS({
-        files: ["focus-mode-mdn.css"],
-        target: { tabId: tab.id },
-      });
-    } else if (nextState === "OFF") {
-      // Remove the CSS file when the user turns the extension off
-      await chrome.scripting.removeCSS({
-        files: ["focus-mode-mdn.css"],
-        target: { tabId: tab.id },
-      });
-    }
+  } else if (nextState === "OFF") {
+    // Remove the CSS file when the user turns the extension off
+    await chrome.scripting.removeCSS({
+      files: [`focus-mode-${domain}.css`],
+      target: { tabId: tab.id },
+    });
   }
 });
-{/* <a href="https://www.flaticon.com/free-icons/zen" title="zen icons">Zen icons created by NeXore88 - Flaticon</a> */}
+/* <a href="https://www.flaticon.com/free-icons/zen" title="zen icons">Zen icons created by NeXore88 - Flaticon</a> */
